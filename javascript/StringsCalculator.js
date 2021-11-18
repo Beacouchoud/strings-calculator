7753be89a462791d9228b69642150e44d8bda9cd
Bea <34965770+Beacouchoud@users.noreply.github.com>
2021-11-18 15:17:14 +0100

    Step 4 with the test passed

--- javascript/StringsCalculator.js
+++ javascript/StringsCalculator.js
@@ -5,8 +5,10 @@ class StringsCalculator  {
   }
 
   add(input) {
+    // this implementation provides a broader support of delimiters, any non-number character is a delimeter
+    let delimiter = /[^0-9]+/i;
     if (!input) this.numbers.push(0);
-    else this.numbers = [...this.numbers, ...input.split(/[,\n]/)]
+    else this.numbers = [...this.numbers, ...input.split(delimiter)]
   }
 
   calculate() {
