789dc9e8bcc768a25506e6c6809eaebe4f1e60bd
Bea <34965770+Beacouchoud@users.noreply.github.com>
2021-11-18 17:13:27 +0100

    Final commit

--- javascript/StringsCalculator.js
+++ javascript/StringsCalculator.js
@@ -7,23 +7,23 @@ class StringsCalculator  {
 
   add(input) {
     // this implementation provides a broader support of delimiters, any non-number character is a delimeter
-    let delimiter = /[^-0-9]+/i;
+    const delimiter = /[^-0-9]+/i;
     if (!input) this.numbers.push(0);
-    else this.numbers = [...this.numbers, ...input.split(delimiter)]
-    console.log(this.numbers)
+    else this.numbers = [...this.numbers, ...input.split(delimiter)];
+    console.log("Input values: ", this.numbers);
   }
 
   calculate() {
-   if (this.handleNegativeNumbers()) {
-    throw Error('negatives not allowed: ' + this.negativeNumbers.join(' '));
-   } else {
+   if (this.hasNegativeNumbers()) {
+    throw Error(`Negatives not allowed:  ${this.negativeNumbers.join(' ')}`);
+   }
     return this.numbers
                 .filter(n => n <= 1000)
-                .reduce((previous, current) => Number(previous) + Number(current), 0);
-    }
+                .reduce((prev, curr) => Number(prev) + Number(curr), 0);
+  
   }
 
-  handleNegativeNumbers() {
+  hasNegativeNumbers() {
     this.negativeNumbers = this.numbers.filter(n => n < 0);
     return (this.negativeNumbers.length===0) ? false : true;
   }
@@ -32,5 +32,5 @@ class StringsCalculator  {
 module.exports = StringsCalculator;
 
 let calc = new StringsCalculator();
-calc.add("1,4;1/;2000\n3");
-console.log(calc.calculate());
+calc.add("1,4;-1/;2000\n3");
+console.log("Result: ", calc.calculate());
