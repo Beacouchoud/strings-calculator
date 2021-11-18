e5fd342f75dec710b409856811ba3a7fc139d3ae
Bea <34965770+Beacouchoud@users.noreply.github.com>
2021-11-18 14:52:47 +0100

    Step 1 tests

--- javascript/StringsCalculator.test.js
+++ javascript/StringsCalculator.test.js
@@ -10,4 +10,24 @@ describe('StringsCalculator', () => {
 
     expect(result).toBe(0);
   });
+  
+  it.skip('works with 1 number', () => {
+    const stringsCalculator = new StringsCalculator();
+
+    stringsCalculator.add('1');
+
+    const result = stringsCalculator.calculate();
+
+    expect(result).toBe(1);
+  });
+
+  it.skip('works with 2 numbers', () => {
+    const stringsCalculator = new StringsCalculator();
+
+    stringsCalculator.add('1,2');
+
+    const result = stringsCalculator.calculate();
+
+    expect(result).toBe(3);
+  });
 });
